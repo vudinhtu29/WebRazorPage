4777dbe154fe430f9de35f47e09598d06ab40747
Tu Vu Dinh <133783670+vudinhtu29@users.noreply.github.com>
2025-01-15 09:21:38 +0700

    tuvd
    010203.003

---
+++ Bloggie/BloggieWeb/Data/BloogieDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BloggieWeb.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggieWeb.Data
+{
+    public class BloogieDbContext : DbContext
+    {
+        public BloogieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
